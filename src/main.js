c1880b2ed3c867086bc210ec5ef3856fc409574d
Jirat Arinrith <jepezi@gmail.com>
2016-02-19 09:50:32 +0700

    refactor into <Nav /> and <Footer />

--- src/main.js
+++ src/main.js
@@ -1,39 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return <div>
-      <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div className="container-fluid">
-          <div className="navbar-header page-scroll">
-            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-          </div>
-
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav navbar-right">
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <a href="about.html">About</a>
-              </li>
-              <li>
-                <a href="post.html">Sample Post</a>
-              </li>
-              <li>
-                <a href="contact.html">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Nav />
 
       <header className="intro-header" style={{backgroundImage: "url('img/home-bg.jpg')"}}>
         <div className="container">
@@ -108,41 +81,7 @@ class App extends Component {
 
       <hr />
 
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-              <ul className="list-inline text-center">
-                <li>
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <span className="fa-stack fa-lg">
-                      <i className="fa fa-circle fa-stack-2x"></i>
-                      <i className="fa fa-github fa-stack-1x fa-inverse"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-              <p className="copyright text-muted">Copyright &copy; Your Website 2014</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>;
   }
 }
