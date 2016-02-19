419a13e9c19e0a7370f7f68ebc4c5ce346cdc97f
Jirat Arinrith <jepezi@gmail.com>
2016-02-19 15:55:52 +0700

    try redux

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const reducers = (state = [], action) => {
+  if (action.type === 'greeting') {
+    return [ ...state, action.name ];
+  }
+
+  return state;
+}
+
+const store = createStore(reducers);
+store.dispatch({ type: 'greeting', name: 'Jip' });
+store.dispatch({ type: 'greeting', name: 'Luna' });
+console.warn(store.getState());
+
 ReactDOM.render(
   <Router
     history={browserHistory}
